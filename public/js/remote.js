305bf0a42868559b43905123f67917334c1c8492
Joel <joelalovera@gmail.com>
2014-05-12 23:48:07 -0400

    add another functions

--- public/js/remote.js
+++ public/js/remote.js
@@ -1,8 +1,8 @@
 var Remote = {
     init: function(){
-        Remote.listeners();
+        Remote.listenControl();
     },
-    listeners: function(){
+    listenControl: function(){
         $('#next').on('click', function(){
             Remote.events.next();
         });
@@ -10,22 +10,58 @@ var Remote = {
         $('#prev').on('click', function(){
             Remote.events.prev();
         });
+
+        $('#f').on('click', function(){
+            Remote.events.fullScreen();
+        });
+
+        $('#esc').on('click', function(){
+            Remote.events.esc();
+        });
+
+        $('#h').on('click', function(){
+            Remote.events.h();
+        });
+
+        $('#play').on('click', function(){
+            Remote.events.play();
+        })
     },
     events: {
         next: function(){
-            socket.emit('remoteEvent', {action:'next'});
+            socket.emit('remoteEvent', {action:'next', code: 37});
         },
         prev: function(){
-            socket.emit('remoteEvent', {action:'prev'});
+            socket.emit('remoteEvent', {action:'prev', code: 39});
         },
         fullScreen: function(){
-            socket.emit('remoteEvent', {action:'fullScreen'});
+            socket.emit('remoteEvent', {action:'fullScreen', code: 70});
         },
         esc: function(){
-            socket.emit('remoteEvent', {action:'esc'});
+            socket.emit('remoteEvent', {action:'esc', code: 27});
         },
         h: function(){
-            socket.emit('remoteEvent', {action:'h'});
+            socket.emit('remoteEvent', {action:'h', code: 72});
+        },
+        play: function(){
+            socket.emit('remoteEvent', {action:'play', code: 0});
         }
+    },
+    initSlides: function(socket){
+        Remote.listenSlide(socket);
+    },
+    listenSlide: function(socket){
+        socket.on('connect', function(){
+        socket.on('actionEvent', function(data){
+                if(data.code === 0){
+                    document.querySelector('video').play()
+                }else{
+                    var evt = document.createEvent('Event');
+                    evt.initEvent('keydown', true, true);
+                    evt.keyCode = data.code;
+                    document.dispatchEvent(evt);
+                }
+            });
+        });
     }
 }
